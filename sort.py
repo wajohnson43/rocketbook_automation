fad06d8cb3242cd41b625d62e4c22ee8b647f845
Will Johnson <wajohnson4342@gmail.com>
2021-02-06 18:57:45 -0600

    Initial commit

---
+++ sort.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import os
+import glob
+import time
+from PyPDF2 import PdfFileMerger
+
+path = '/home/scifr/Documents/notes/'
+
+def sort():
+
+    debug = False
+
+    for filename in glob.glob(path + '*.pdf'):
+        fileparts = filename.split('_', 1)
+        directory = fileparts[0]
+        name = fileparts[1]
+        
+        if(debug):
+            print("directory: " + directory)
+            print("name: " + name)
+
+        try:
+            os.mkdir(directory)
+            os.rename(filename, directory + "/" + name)
+        except:
+            os.rename(filename, directory + "/" + name)
+    return
+
+def merge():
+    for directory in glob.glob(path + '*/'):
+        pdfs = glob.glob(directory + '*.pdf')
+        
+        if len(pdfs) == 1:
+            continue
+
+        pdfs.sort(key=os.path.getmtime)
+        
+        merger = PdfFileMerger()
+
+        for pdf in pdfs:
+            merger.append(open(pdf, 'rb'))
+            os.remove(pdf)
+        notebook = directory.split('/')[-2]
+
+        with open(directory + notebook + '.pdf', 'wb') as fout:
+            merger.write(fout)
+
+def main():
+    sort()
+    merge()
+
+if __name__ == '__main__':
+    main()
